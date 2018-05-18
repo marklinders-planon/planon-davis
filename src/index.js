015ecea673542c737511036a5e42d856a96fc691
kjansen <kristiaanjansen@gmail.com>
2018-05-18 16:22:24 +0200

    added multiple messages handeling. (still figuring out how sqs works). Added authentication to make sure only our davis calls this lambda

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-let AWS = require('aws-sdk');
-AWS.config.update({region: 'eu-west-1'});
+var AWS;
+if (!AWS) {
+    AWS = require('aws-sdk');
+}
+AWS.config.update({region: 'eu-west-1'})
+const config = require("./config");
 
 async function getAccountNumber(payload) {
   var sts = new AWS.STS();
@@ -13,28 +17,55 @@ async function getsqs() {
   var params = {
     QueueUrl: queueURL,
     AttributeNames: ['all'],
-    MaxNumberOfMessages: 1,
+    MaxNumberOfMessages: 10,
     MessageAttributeNames: ['all'],
     VisibilityTimeout: 0,
     WaitTimeSeconds: 20
   };
-  return await sqs.receiveMessage(params).promise(); 
+  var messages = await sqs.receiveMessage(params).promise();
+  var foundMessageIds = new Array;
+  var uniqeMessages = new Array;
+  for (let i=0; i<messages.Messages.length; i++){
+    var found = foundMessageIds.find(function(element) {
+      return element === messages.Messages[i].MessageId;
+    });
+    if (!found) {
+      foundMessageIds.push(messages.Messages[i].MessageId);
+      uniqeMessages.push(messages.Messages[i]);
+    }
+  }
+  return uniqeMessages; 
 }
 
 async function handleCommands(payload) {
-  switch(payload.text) {
-    case 'cockpit':
-        var sqs = await getsqs();
-        var body = JSON.parse(sqs.Messages[0].Body);   
-        return '{ "text": "Origin: ' + body.origin + '. ' + 'Occured: ' + body.occured + '. ' + body.message + '. ", "card": { "color": "red" }';
-    case 'help':
-        return '{"text": "for example say give me my cockpit incidents!"}';
-    default:
-        return '{"text": "Sorry, I don`t know: ' + payload.text + '."}';
+  if (!payload.text.indexOf("cockpit") || !payload.text.indexOf("give")) {
+    var messages = await getsqs();
+    var responseText = " ";
+    for (let i=0; i<messages.length; i++){
+      var body = JSON.parse(messages[i].Body);   
+      var incidentNR = i + 1;
+      responseText = responseText + 'Incident number : '+incidentNR+'. ' + 'Origin:'  + body.origin + '. ' + 'Occured: ' + body.occured + '. ' + body.message + '. ';
+    }
+    return { "text": responseText , "card": { "color": "red" } };
+  } else if  (!payload.text.indexOf("help")) {
+    return {"text": "for example say give me my cockpit incidents!"};
+  } else if  (!payload.text.indexOf("no")) {
+    return {"text": "Say stop to exit"};
+  } else if  (!payload.text.indexOf("planon")) {
+    return {"text": "Planon is the greatest company in the world"};
+  } else if  (!payload.text.indexOf("mark") || !payload.text.indexOf("kris")) {
+    return {"text": "i don't know. I do know Mark and Kris made this stuff"};
+  } else {
+    return {"text": "Sorry, I don`t know: " + payload.text + "."};
   }
 }
 
 module.exports.handler = async (event, context, callback) => {
+  //check if we are authorized
+  if (!event.headers || event.headers.Authorization !== `Token ${config.CLIENT_SECRET}`) {
+    return callback(null, { statusCode: 401, body: "Unauthorized" });
+  }
+
   console.log(event);  
   try {
     var body = JSON.parse(event.body);
@@ -48,7 +79,7 @@ module.exports.handler = async (event, context, callback) => {
           confirmation: false,
           response: repsonseFromCommands
         })
-      });      
+      });
     }
   }
   catch(error) {
